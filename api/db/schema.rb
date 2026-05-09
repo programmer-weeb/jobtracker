7e4ca4db220bcae1900ed26aa7c8adae082395c6
Ahmed Melouk <forprogramming8@gmail.com>
2026-05-09 17:56:04 +0300

    chore(db): commit schema after recoverable/rememberable removal

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_09_133000) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_09_143457) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -91,13 +91,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_09_133000) do
     t.string "encrypted_password", default: "", null: false
     t.string "jti", null: false
     t.string "name"
-    t.datetime "remember_created_at"
-    t.datetime "reset_password_sent_at"
-    t.string "reset_password_token"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "application_tags", "applications"
